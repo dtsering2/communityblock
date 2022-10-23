aa5fbb36aed7031a81f84841daef3c73a4a23c4b
Tsering Tsering <dtsering2@gmail.com>
2022-10-23 19:29:58 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require ("express"); //express allows us to create the server
+const http = require("http");
+const cors = require("cors"); //our frontend and backend are in 2 different ports, so we need cors to allow communication between the two
+const mongoose = require("mongoose"); //we will create our db with mongoose
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || process.env.API_PORT;
+
+const app = express(); //creating an instance of an express server
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+const server = http.createServer(app);
+
+//connection to our db
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    server.listen(PORT, ()=>{
+        console.log(`Server is on ${PORT}`)
+    });
+})
+.catch(err=>{
+    console.log('db connection failed. Server not start.');
+    console.error(err);
+})
